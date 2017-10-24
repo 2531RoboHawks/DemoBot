a76e4dd53f33e17a66cf8110a9ebb0a67d0018d3
robohawks <robohawks@robohawks-Latitude-E6420>
2017-10-24 16:16:31 -0500

    commit

---
+++ DemoBot/src/org/usfirst/frc/team2531/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team2531.robot.subsystems;
+
+import org.usfirst.frc.team2531.robot.commands.ExampleCommand;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ExampleSubsystem extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	CANTalon motorFL = new CANTalon(2);
+	CANTalon motorFR = new CANTalon(1);
+	CANTalon motorBL = new CANTalon(4);
+	CANTalon motorBR = new CANTalon(3);
+
+	public ExampleSubsystem() {
+
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+
+		setDefaultCommand(new ExampleCommand());
+	}
+
+	public void tankDrive(Joystick joystick) {
+		double x = joystick.getRawAxis(0);
+		double y = joystick.getRawAxis(1);
+		double z = joystick.getRawAxis(2);
+		double left = (y + x) * z;
+		double right = (y - x) * z;
+		motorFL.set(left);
+		motorBL.set(left);
+		motorFR.set(right);
+		motorBR.set(right);
+	}
+}
